841e3b5f2c9d8b02950b6c98ab677bc6f08e018f
AmirM <Amir.mule@rediffmail.com>
2021-12-13 20:08:51 +0530

    Created Controller & Repository.

---
+++ Amtech.BookStore/Amtech.BookStore/Controllers/BookController.cs
@@ -0,0 +1,31 @@
+﻿using Amtech.BookStore.Models;
+using Amtech.BookStore.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Amtech.BookStore.Controllers
+{
+    public class BookController:Controller
+    {
+        private readonly BookRepository _book = null;
+        public BookController()
+        {
+            _book = new BookRepository();
+        }
+        public List<Book> GetBooks()
+        {
+            return _book.GetAllBooks();
+        }
+        public Book GetBook(int id)
+        {
+            return _book.GetBookByid(id);
+        }
+        public List<Book> SearchBooks(string author, String bookName)
+        {
+            return _book.SearchBooks( author, bookName);
+        }
+    }
+}
---
+++ Amtech.BookStore/Amtech.BookStore/Repository/BookRepository.cs
@@ -0,0 +1,35 @@
+﻿using Amtech.BookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Amtech.BookStore.Repository
+{
+    public class BookRepository
+    {
+        public List<Book> GetAllBooks()
+        {
+            return GetBooksData();
+        }
+        public Book GetBookByid(int id)
+        {
+            return GetBooksData().Where(a => a.Id == id).FirstOrDefault();
+        }
+        public List<Book> SearchBooks(string author,string name )
+        {
+            return GetBooksData().Where(a => a.Name.Contains(name) || a.author.Contains(author)).ToList();
+        }
+        private List<Book> GetBooksData()
+        {
+            return new List<Book>
+            {
+                new Book{Id=100,Name="Mvc",author="Raymond C"},
+                new Book{Id=101,Name="Dot Net Core Mvc",author="Raymond C"},
+                new Book{Id=102,Name="Java",author="Patric"},
+                new Book{Id=103,Name="PHP",author="Hammand"},
+                new Book{Id=104,Name="JavaScript",author="Lui Chrown"}
+            };
+        }
+    }
+}
